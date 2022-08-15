dbd04afae60c13eb494c7ceb75285647716874f5
Kevin <kevdev.yr6re@slmail.me>
2022-08-15 01:16:04 -0700

    added kata

---
+++ 7.Scrolling Text/script.js
@@ -0,0 +1,9 @@
+function scrollingText(text){
+  let arr = []
+  for (let i=0; i<text.length; i++) {
+    arr.push(
+      (text.slice(i) + text.slice(0, i)).toUpperCase()
+    )
+  }
+  return arr
+}
