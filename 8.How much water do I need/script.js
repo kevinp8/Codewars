98391f9421ac98b206bde80e1b090da7ed4b4b60
Kevin <kevdev.yr6re@slmail.me>
2022-09-09 00:35:33 -0700

    added kata

---
+++ 8.How much water do I need/script.js
@@ -0,0 +1,7 @@
+function howMuchWater(water, load, clothes){
+  if (clothes > 2*load) return 'Too much clothes'
+  else if (clothes < load) return 'Not enough clothes'
+  else {
+    return Number((water * 1.1**(clothes - load)).toFixed(2))
+  }
+}
