58148c879fc156e133cfbebfaf6f549ea141e39d
Kevin <kevdev.yr6re@slmail.me>
2022-11-27 00:23:50 -0800

    added kata

---
+++ 8.Training JS #13 Number object and its properties/script.js
@@ -0,0 +1,11 @@
+function whatNumberIsIt(n){
+  if (n === Number.POSITIVE_INFINITY) return "Input number is Number.POSITIVE_INFINITY"
+  else if (n === 100) return "Input number is 100"
+  else if (n === Number.MAX_VALUE) return "Input number is Number.MAX_VALUE"
+  else if (n === Number.MIN_VALUE) return "Input number is Number.MIN_VALUE"
+  else if (n === Number.NEGATIVE_INFINITY) return "Input number is Number.NEGATIVE_INFINITY"
+  else if (!n) return "Input number is Number.NaN"
+  else return `Input number is ${n}`
+ 
+  
+}
